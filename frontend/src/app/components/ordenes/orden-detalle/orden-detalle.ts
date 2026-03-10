7c56f3e6319a0a68f58bea9a69aa812801b0cf6f
2025-alejandro-UMH-Design <202401275@umh.edu.hn>
2026-03-10 00:45:42 -0600

    feat(frontend): crear estructura inicial de carpetas y archivos

---
+++ frontend/src/app/components/ordenes/orden-detalle/orden-detalle.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orden-detalle',
+  imports: [],
+  templateUrl: './orden-detalle.html',
+  styleUrl: './orden-detalle.css',
+})
+export class OrdenDetalle {}
---
+++ frontend/src/app/components/proveedores/proveedor-form/proveedor-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proveedor-form',
+  imports: [],
+  templateUrl: './proveedor-form.html',
+  styleUrl: './proveedor-form.css',
+})
+export class ProveedorForm {}
